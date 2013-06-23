06907c4f531a38f72fef0167e572b5cd393525cc
gyk001 <gyk001@gmail.com>
2013-06-23 12:08:41 +0800

    初次提交

---
+++ src/test/java/sy/controller/UserControllerTest.java
@@ -0,0 +1,11 @@
+package sy.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guo
+ * Date: 13-6-23
+ * Time: 上午9:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserControllerTest {
+}
